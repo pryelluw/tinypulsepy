8ecc02a6bb9c43f178f15ad9a4b4f54a0dc26e32
privera <privera@celerity.com>
2020-02-12 15:52:48 -0500

    unit tests for get_cheers WIP

--- src/tests.py
+++ src/tests.py
@@ -1,7 +1,9 @@
 # python -m unittest -v tests.TestShortcuts
 import unittest
 import mock
+import requests
 from client import TinypulseAPIClient
+import constants
 from shortcuts import get_cheers
 
 
@@ -20,14 +22,27 @@ def mock_get(*args, **kwargs):
     return MockResponse(200, {'data': []})
 
 
-
 class TestShortcuts(unittest.TestCase):
     
     
     @mock.patch('requests.get', side_effect=mock_get)
     def test_get_cheers_response_ok(self, mocko):
         self.assertEqual(get_cheers('12345'), {'data': []})
-        
+
+
+class TestAPIEndpoints(unittest.TestCase):
+    
+    def setUp(self):
+        self.client = TinypulseAPIClient('12345', access_token=None)
+
+    @mock.patch('requests.get', side_effect=mock_get)
+    def test_get_cheers_calls_api_all_cheers_endpoint(self, mocko):
+        self.client.get(constants.CHEERS)
+        mocko.assert_called_with(
+            '{}{}'.format(self.client.BASE_URL,constants.CHEERS),
+            headers={'AccessToken': self.client._api_key}
+        )
+    
         
 if __name__ == '__main__':
     unittest.main()
