8fdf0480f1ed28e4dd5d4e8df1eeaa283da65e3f
privera <privera@celerity.com>
2020-03-09 14:08:37 -0400

    rename and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='tinypulsepy',
+      version='0.1.0',
+      description='API client for TinyPulse API V1.',
+      url='https://github.com/pabloriveracelerity/tinypulsepy/',
+      author='Pablo Rivera',
+      author_email='privera@celerity.com',
+      license='MIT',
+      packages=['tinypulsepy'],
+      install_requires=['requests==2.22.0', 'mock==3.0.5'],
+      zip_safe=False,
+      download_url="https://github.com/pabloriveracelerity/tinypulsepy/archive/0.1.0.tar.gz",
+      keywords=["Tiny Pulse", "Tiny Pulse API", "API", "Celerity"],
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          ],
+      )
