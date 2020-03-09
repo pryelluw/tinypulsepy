1065d534b5cc49f22012320f52dab90773974a09
privera <privera@celerity.com>
2020-03-09 19:07:07 -0400

    bumped releae

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tinypulsepy',
-      version='0.1.0',
+      version='0.2.0',
       description='API client for TinyPulse API V1.',
       url='https://github.com/pabloriveracelerity/tinypulsepy/',
       author='Pablo Rivera',
@@ -10,7 +10,7 @@ setup(name='tinypulsepy',
       packages=['tinypulsepy'],
       install_requires=['requests==2.22.0', 'mock==3.0.5'],
       zip_safe=False,
-      download_url="https://github.com/pabloriveracelerity/tinypulsepy/archive/0.1.0.tar.gz",
+      download_url="https://github.com/pabloriveracelerity/tinypulsepy/archive/0.2.0.tar.gz",
       keywords=["Tiny Pulse", "Tiny Pulse API", "API", "Celerity"],
       classifiers=[
           "License :: OSI Approved :: MIT License",
